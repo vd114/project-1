2eb4ab6d2f594f97318df15e4ae8b9514c646406
Vivek Dixit <vivekhpc2@gmail.com>
2018-05-01 08:07:28 -0500

    add question to the q1.py file

--- q1.py
+++ q1.py
@@ -1,3 +1,10 @@
+"""
+Question 1
+
+Given two strings s and t, determine whether some anagram of t is a substring of s. For example: if s = "udacity" and t = "ad", then the function returns True. Your function definition should look like: question1(s, t) and return a boolean True or False.
+
+"""
+
 def question1(s, t):
     len_t = len(t)
     len_s = len(s)
